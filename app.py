b13e4e65db4d01d8d97c010ba4d46467ab836878
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-06-07 23:03:18 +0200

    gardening

--- app.py
+++ app.py
@@ -95,26 +95,19 @@ def create_app(settings=None):
 
 
 def create_freezer(*args, **kwargs):
-    # add url generators here
     freezer = Freezer(create_app(*args, **kwargs))
     return freezer
 
 
 def parse_args(argv=None):
-    parser = argparse.ArgumentParser(description=__doc__)
+    parser = argparse.ArgumentParser()
     parser.add_argument('--debug', '-d', action='store_true', dest='DEBUG')
     subparsers = parser.add_subparsers(title='commands')
 
-    parser_build = subparsers.add_parser(
-        'build', help='generate static sites')
-    parser_build.add_argument(
-        '--destination', '-d', default=None,
-        help='directory where Flekky will write files '
-               '(default: <source>_build)')
+    parser_build = subparsers.add_parser('build', help='generate static HTML')
     parser_build.set_defaults(cmd='build')
 
-    parser_serve = subparsers.add_parser(
-        'serve', help='run a test server for development')
+    parser_serve = subparsers.add_parser('serve', help='run a test server')
     parser_serve.add_argument('--port', '-p', type=int, default=8000)
     parser_serve.set_defaults(cmd='serve')
 

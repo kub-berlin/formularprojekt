f0bf01c6819e14a77fc1b23692368fa04b1d6cb1
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-08-16 17:10:54 +0200

    add script for filling translations from other forms

---
+++ fill.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+translations = {}
+forms = {}
+
+BASEPATH = 'data'
+
+def iter_translations():
+    for dirpath, dirnames, filenames in os.walk(BASEPATH):
+        for filename in filenames:
+            form_id = os.path.basename(dirpath)
+            path = os.path.join(dirpath, filename)
+
+            if filename.endswith('.json'):
+                lang_id = filename[:-5]
+
+                yield form_id, lang_id, path
+
+
+for form_id, lang_id, path in iter_translations():
+    if form_id != 'meta':
+        with open(path) as fh:
+            try:
+                data = json.load(fh)
+            except:
+                print(path)
+                raise
+
+        if lang_id == 'form':
+            forms[form_id] = data
+        else:
+            if lang_id not in translations:
+                translations[lang_id] = {}
+            translations[lang_id].update(data)
+
+
+for form_id, form in forms.items():
+    for lang_id, translation in translations.items():
+        data = {}
+
+        keys = set([r[1] for r in form['rows']])
+        for key in keys:
+            if key in translation:
+                data[key] = translation[key]
+
+        if data:
+            path = os.path.join(BASEPATH, form_id, lang_id + '.json')
+            with open(path, 'w') as fh:
+                json.dump(data, fh, indent=2, separators=(',', ': '), sort_keys=True)
+
+
+# for form_id, lang_id, path in iter_translations():
+#     with open(path) as fh:
+#         data = json.load(fh)
+#     with open(path, 'w') as fh:
+#         json.dump(data, fh, indent=2, separators=(',', ': '), sort_keys=True)

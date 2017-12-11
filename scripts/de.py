0af0d9e48bf31b0fc7e2a177047cb437aa0255e9
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-12-11 18:56:32 +0100

    make de script work with csv

--- scripts/de.py
+++ scripts/de.py
@@ -1,15 +1,18 @@
-import json
 import sys
+import csv
+import json
 
-
-ensure_str = lambda s: s if isinstance(s, str) else s.encode('utf8')
-f = lambda row: ensure_str(row['content'])
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as fh:
         data = json.load(fh)
 
-    odata = {f(row): f(row) for row in data['rows'] if row['content']}
+    keys = []
 
     with open(sys.argv[2], 'w') as fh:
-        json.dump(odata, fh, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
+        w = csv.writer(fh)
+        for row in data['rows']:
+            key = row.get('content')
+            if key and key not in keys:
+                keys.append(key)
+                w.writerow((key, key))

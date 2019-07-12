a75a3b077a74a048abb797f2e1ceeb586a4d18a8
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-07-12 16:55:51 +0200

    Revert "add bilingual scripts"
    This reverts commit cc839c4c8cb901bc0fa0b154989994d8eb6a41b7.

--- scripts/bilingual.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-form=$(basename $1 | sed 's/_.*//')
-output=$(echo $1 | sed 's/\.pdf/_bilingual\.pdf/')
-pdftk A="$1" B=".upstream/$form.pdf" shuffle A B output "$output"
--- scripts/rename_bilingual.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ls *_bilingual.pdf | while read f; do mv "$f" "$(echo $f | sed 's/_bilingual//')"; done
